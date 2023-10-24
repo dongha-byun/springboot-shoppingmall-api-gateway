f444a89afef98f92dbed270b903a7eee79b70ee3
dongha-byun <byunsw4@naver.com>
2023-10-24 23:55:45 +0900

    feat: API Gateway 설정 초안

--- src/main/java/shoppingmall/apigateway/GatewayConfiguration.java
+++ src/main/java/shoppingmall/apigateway/GatewayConfiguration.java
@@ -1,25 +1,64 @@
 package shoppingmall.apigateway;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import reactor.core.publisher.Mono;
 import shoppingmall.apigateway.filters.JwtAuthorizationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 public class GatewayConfiguration {
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("main-service", predicateSpec -> predicateSpec
-                        .path("/**")
-                        .filters(gatewayFilterSpec -> gatewayFilterSpec
-                                .filter(new JwtAuthorizationFilter())
-                                .addResponseHeader("X-SHOP-HEADER", "DONG-HA")
+                .route("user-service", predicate -> predicate
+                        .path("/user-service/sign-up")
+                        .and().method(HttpMethod.POST)
+                        .filters(gatewayFilter -> gatewayFilter
+                                .removeRequestHeader(HttpHeaders.COOKIE)
+                                .rewritePath("/user-service/(?<segment>.*)", "/$\\{segment}")
                         )
-                        .uri("lb://MAIN-SERVICE")
+                        .uri("lb://USER-SERVICE")
+                )
+                .route("user-service", predicate -> predicate
+                        .path("/user-service/find-email", "/user-service/find-pw", "/user-service/login")
+                        .and().method(HttpMethod.POST)
+                        .filters(gatewayFilter -> gatewayFilter
+                                .removeRequestHeader(HttpHeaders.COOKIE)
+                                .rewritePath("/user-service/(?<segment>.*)", "/$\\{segment}"))
+                        .uri("lb://USER-SERVICE")
+                )
+                .route("user-service", predicate -> predicate
+                        .path("/user-service/users/**")
+                        .and().method(HttpMethod.PUT, HttpMethod.GET)
+                        .filters(gatewayFilter -> gatewayFilter
+                                .removeRequestHeader(HttpHeaders.COOKIE)
+                                .rewritePath("/user-service/(?<segment>.*)", "/$\\{segment}")
+                                .filter(new JwtAuthorizationFilter()))
+                        .uri("lb://USER-SERVICE")
                 )
                 .build();
+    }
+
+    @AllArgsConstructor
+    @Getter
+    static class OriginRequestBody {
+        private String email;
+        private String password;
+    }
+
+    @AllArgsConstructor
+    static class NewRequestBody {
+        private String email;
+        private String password;
     }
 }
--- src/main/java/shoppingmall/apigateway/filters/JwtAuthorizationFilter.java
+++ src/main/java/shoppingmall/apigateway/filters/JwtAuthorizationFilter.java
@@ -1,17 +1,34 @@
 package shoppingmall.apigateway.filters;
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Slf4j
+@Component
 public class JwtAuthorizationFilter implements GatewayFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("JwtAuthorizationFilter's filter start");
+        ServerHttpRequest request = exchange.getRequest();
+        List<String> authorizations = request.getHeaders().get(HttpHeaders.AUTHORIZATION);
+
+        log.info("authorizations = {}", authorizations);
+        if(authorizations == null || authorizations.isEmpty()) {
+            throw new IllegalArgumentException("인증 정보가 존재하지 않습니다.");
+        }
+
+        for (String header : authorizations) {
+
+        }
+
         return chain.filter(exchange);
     }
 }
