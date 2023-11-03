52a908d24bdd1f49a86a23fe9efc5a9126b142b4
dongha-byun <byunsw4@naver.com>
2023-11-03 23:52:31 +0900

    feat: api gateway 에서 jwt validation check

--- src/main/java/shoppingmall/apigateway/filters/JwtAuthorizationFilter.java
+++ src/main/java/shoppingmall/apigateway/filters/JwtAuthorizationFilter.java
@@ -1,34 +1,65 @@
 package shoppingmall.apigateway.filters;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.List;
+import javax.crypto.SecretKey;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Slf4j
-@Component
 public class JwtAuthorizationFilter implements GatewayFilter {
+
+    private final SecretKey secretKey =
+            Keys.hmacShaKeyFor("secret_key_of_dong_ha_do_not_snap_this".getBytes(StandardCharsets.UTF_8));
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         List<String> authorizations = request.getHeaders().get(HttpHeaders.AUTHORIZATION);
 
-        log.info("authorizations = {}", authorizations);
         if(authorizations == null || authorizations.isEmpty()) {
             throw new IllegalArgumentException("인증 정보가 존재하지 않습니다.");
         }
 
-        for (String header : authorizations) {
+        for (String authorization : authorizations) {
+            if(authorization.startsWith("Bearer")) {
+                String jwtToken = authorization.replace("Bearer", "").trim();
+                try{
+                    Date expiration = Jwts.parser().verifyWith(secretKey)
+                            .build()
+                            .parseSignedClaims(jwtToken)
+                            .getPayload()
+                            .getExpiration();
 
+                    if(expiration.before(new Date())) {
+                        throw new IllegalStateException("인증 정보가 만료됐습니다.");
+                    }
+
+                    String subject = Jwts.parser().verifyWith(secretKey)
+                            .build()
+                            .parseSignedClaims(jwtToken)
+                            .getPayload()
+                            .getSubject();
+                    exchange.getRequest().mutate().header("X-GATEWAY-EMAIL", subject);
+
+                }catch(Exception e1){
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
+                }
+            }
         }
 
         return chain.filter(exchange);
     }
+
 }
