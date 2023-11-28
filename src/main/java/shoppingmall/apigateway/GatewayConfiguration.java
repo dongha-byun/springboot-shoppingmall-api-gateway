871dab7f31207ff7765e8dc5198915cef3fc1794
dongha-byun <byunsw4@naver.com>
2023-11-28 22:10:04 +0900

    feat: 파트너즈 로그인 라우팅 분리

--- src/main/java/shoppingmall/apigateway/GatewayConfiguration.java
+++ src/main/java/shoppingmall/apigateway/GatewayConfiguration.java
@@ -18,44 +18,53 @@ public class GatewayConfiguration {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("user-service-1", predicate -> predicate
+                .route("user-service-1", predicateSpec -> predicateSpec
                         .path("/user-service/sign-up",
                                 "/user-service/find-email",
                                 "/user-service/find-pw",
                                 "/user-service/login"
                         )
                         .and().method(HttpMethod.POST)
-                        .filters(gatewayFilter -> gatewayFilter
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
                                 .removeRequestHeader(HttpHeaders.COOKIE)
                                 .rewritePath("/user-service/(?<segment>.*)", "/$\\{segment}"))
                         .uri("lb://USER-SERVICE")
                 )
-                .route("user-service-2", predicate -> predicate
-                        .path("/user-service/health-check")
+                .route("user-service-2", predicateSpec -> predicateSpec
+                        .path("/user-service/health-check", "/user-service/test-cookie")
                         .and().method(HttpMethod.GET)
-                        .filters(gatewayFilter -> gatewayFilter
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
                                 .removeRequestHeader(HttpHeaders.COOKIE)
                                 .rewritePath("/user-service/(?<segment>.*)", "/$\\{segment}"))
                         .uri("lb://USER-SERVICE")
                 )
-                .route("user-service-3", predicate -> predicate
+                .route("user-service-3", predicateSpec -> predicateSpec
                         .path("/user-service/users/**")
                         .and().method(HttpMethod.PUT, HttpMethod.GET)
-                        .filters(gatewayFilter -> gatewayFilter
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
                                 .removeRequestHeader(HttpHeaders.COOKIE)
                                 .rewritePath("/user-service/(?<segment>.*)", "/$\\{segment}")
                                 .filter(jwtAuthorizationFilter))
                         .uri("lb://USER-SERVICE")
                 )
-                .route("user-service-4", predicate -> predicate
+                .route("user-service-4", predicateSpec -> predicateSpec
                         .path("/user-service/refresh")
                         .and().method(HttpMethod.GET)
-                        .filters(gatewayFilter -> gatewayFilter
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
                                 .removeRequestHeader(HttpHeaders.COOKIE)
                                 .rewritePath("/user-service/(?<segment>.*)", "/$\\{segment}"))
                         .uri("lb://USER-SERVICE")
                 )
-                .route("main-service-1", predicate -> predicate
+                .route("main-service-1", predicateSpec -> predicateSpec
+                        .path("/main-service/partners/login")
+                        .and().method(HttpMethod.POST)
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
+                                .removeRequestHeader(HttpHeaders.COOKIE)
+                                .rewritePath("/main-service/(?<segment>.*)", "/$\\{segment}")
+                        )
+                        .uri("lb://MAIN-SERVICE")
+                )
+                .route("main-service-2", predicateSpec -> predicateSpec
                         .path("/main-service/**")
                         .filters(gatewayFilterSpec -> gatewayFilterSpec
                                 .removeRequestHeader(HttpHeaders.COOKIE)
